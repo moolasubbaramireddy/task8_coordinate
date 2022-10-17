3f2956e5508d7fedd8f05ed4dcba5e3c2d8125b1
moolasubbaramireddy <subbaramireddym@gmail.com>
2022-10-17 22:05:34 +0530

    div Coordinates funcation adding

---
+++ src/component/Card.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Card = (props) => {
+  const dragStart = (e) => {
+    const traget = e.traget;
+    e.dataTransfer.setData("card_id", traget?.id);
+    // if (typeof dragStart === 'undefined' || !dragStart) {
+    //        return;
+    //     }
+console.log(props)
+    // setTimeout (()=>{
+    //   traget.style.display = "none"
+    // },0)
+  };
+  const dragOver = (e) => {
+    e.stopPropagation();
+  };
+  return (
+    <div
+      id={props.id}
+      className={props.className}
+      draggable={props.draggable}
+      onDragStart={dragStart}
+      onDragOver={dragOver}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/component/Coordinate.js
@@ -0,0 +1,44 @@
+import React ,{useState , useRef} from "react";
+import logo from './assets/bg2.jpg';
+import Card from "./Card";
+import Board from "./Board";
+
+
+const Coordinate = (props) => {
+    const containerRef = useRef();
+  const [coord, setCoord] = useState({ x: 0, y: 0 });
+  const handleMouseMove = (e) => {
+    //clientX, pageX, screenX, offsetX
+    setCoord({
+      x: e.pageX - containerRef.current.getBoundingClientRect().left,
+      y: e.pageY - containerRef.current.getBoundingClientRect().top,
+    });
+  };
+console.log("X="+ " "+coord.x,"Y ="+ " " +coord.y)
+  return (
+    <>
+      <div
+        ref={containerRef}
+        onMouseDown={handleMouseMove}
+        style={{ cursor: 'pointer', height: '415px', width: '624px' }}
+        className="flexbox"
+      >
+        <img
+          src={logo}
+          alt='stadium'
+          height='415px'
+          width='624px'
+        />
+       
+      </div>
+      <Board id='board-1' className='board'>
+        <Card id='card-1' className="card" graggable="true">
+          <p>m</p>
+        </Card>
+       </Board>
+      
+    </>
+  );
+};
+
+export default Coordinate;
